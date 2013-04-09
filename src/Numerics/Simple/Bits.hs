2182957ea983f7ec7fd113a3a24a2717bc9ea645
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2013-04-09 03:24:07 -0400

    now need to write the code to provide the morton z and flip n layouts

--- src/Numerics/Simple/Bits.hs
+++ src/Numerics/Simple/Bits.hs
@@ -87,16 +87,16 @@ note: 32bit Ints give you ~ 4gb of addressable locations PER matrix/array
 so not sure if its worth it
 -}
 
-{-
 
---{-as writ, only shuffles the lower 32 bits, the rest are zerod out
+
+--as writ, only shuffles the lower 32 bits, the rest are zerod out
 ---has good instruction level parallelism  (17 cycles in ideal), but 42 in sequential risc model
 ---- also seems that my 64 bit version is wrong
 
---- -}
+{- previously  -}
 outerShuffle32A :: Word -> Word
 outerShuffle32A !x =
-  case (x .&. 0x0000ff00 ) <<  8 .|. (x >> 8) .&. 0x0000FF00 .|. x  .&. 0xFF0000FF of 
+  case (x .&. 0x0000FF00 ) <<  8 .|. (x >> 8) .&. 0x0000FF00 .|. x  .&. 0xFF0000FF of 
     x -> case ( x .&. 0x00F000F0 ) << 4 .|. (x >> 4) .&. 0x00F000F0 .|. x .&. 0xF00FF00F  of 
       x->case (x .&.  0x0C0C0C0C )<< 2 .|.  (x >> 2) .&. 0x0C0C0C0C .|. x .&. 0xC3C3C3C3 of  
         x-> case ( (x .&. 0x22222222)  << 1  .|. (x>> 1) .&. 0x22222222 .|. x .&. 0x99999999) of 
@@ -104,49 +104,8 @@ outerShuffle32A !x =
 
 
 
--}
-
---mortonOuterShuffle 
-
-{- 64 bit generalization of 32bit outer shuffly from hackers delight
-should add conditional logic so that the step for 64 bit words is dropp
- -}
-{-
----- this code is wrong, not sure where the mistake is
-outerShuffle64A :: Word -> Word
-outerShuffle64A !x =
---- the 16 shift should be conditional
-    case (x .&. 0x00000000FFFF0000) << 16 
-        .|. (x >> 16) .&. 0x00000000FFFF0000 .|. x .&. 0xFFFF00000000FFFF of
-
-      x->case (x .&. 0x0000FF000000FF00 ) <<  8 
-            .|. (x >> 8) .&. 0x0000FF000000FF00 .|. x  .&. 0xFF0000FFFF0000FF of 
-
-        x -> case ( x .&. 0x00F000F000F000F0 ) << 4 
-            .|. (x >> 4) .&. 0x00F000F000F000F0 .|. x .&. 0xF00FF00FF00FF00F  of 
-
-          x->case (x .&.  0x0C0C0C0C0C0C0C0C )<< 2 
-                .|.   (x >> 2) .&. 0x0C0C0C0C0C0C0C0C .|. x .&. 0xC3C3C3C3C3C3C3C3 of  
-
-            x-> case ( (x .&. 0x2222222222222222)  << 1  
-                .|. (x>> 1) .&. 0x2222222222222222 .|. x .&. 0x9999999999999999) of 
-                    res -> res
-{-# INLINE outerShuffle64A #-}
-
--}
 
 
-
-{-
-
-outerUnShuffl code
-
-   t = (x ^ (x >> 1)) & 0x22222222;  x = x ^ t ^ (t << 1);
-   t = (x ^ (x >> 2)) & 0x0C0C0C0C;  x = x ^ t ^ (t << 2);
-   t = (x ^ (x >> 4)) & 0x00F000F0;  x = x ^ t ^ (t << 4);
-   t = (x ^ (x >> 8)) & 0x0000FF00;  x = x ^ t ^ (t << 8);
-
--}
 
 {-
 hacker's delight shuffle code listed 
@@ -159,6 +118,7 @@ flipSidesA x =   (x >> (bitSize x `div` 2  ) ) .|. (x << (bitSize x `div` 2  ))
 
 
 flipSidesB x = x `rotate` (bitSize x `div` 2 )  
+{-#  INLINE flipSidesB #-}
 
 {- 
 for the  32 bit version,
@@ -172,19 +132,23 @@ and what optimizations flags are pased to  llvm
 
 -}
 
+--mortonZ x y = outerShuffle64 $!   (x << (bitSize x `div` 2))  .|.   ( y  .&. () )
+
+
+
 --outerShuffle64B :: Word->Word
-outerShuffle64B :: (Num a, Bits a) => a -> a
-outerShuffle64B !x =
+outerShuffle64 :: (Num a, Bits a) => a -> a
+outerShuffle64 !x =
     case xor2LShift 16 x (xorRShift 16 x  .&.  0xFFFFFFFF0000FFFF) of 
       x -> case xor2LShift  8 x (xorRShift 8 x    .&. 0x0000FF000000FF00)   of 
         x-> case xor2LShift 4 x (xorRShift 4 x .&. 0x00F000F000F000F0) of 
           x->  case xor2LShift 2 x (xorRShift 2 x .&. 0x0C0C0C0C0C0C0C0C) of
             x -> case xor2LShift 1 x (xorRShift 1 x .&. 0x2222222222222222) of 
                 res -> res 
-{-# INLINABLE outerShuffle64B #-}
+{-# INLINABLE outerShuffle64 #-}
 --outerShuffle64B :: Word->Word
-{-# SPECIALIZE outerShuffle64B :: Word->Word #-}
-{-# SPECIALIZE outerShuffle64B :: Int->Int #-}
+{-# SPECIALIZE outerShuffle64 :: Word->Word #-}
+{-# SPECIALIZE outerShuffle64 :: Int->Int #-}
 
 
 outerUnShuffle64 :: (Num a, Bits a) => a -> a
