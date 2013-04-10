b6a29a1966148fdc83fbced31aed0ab7681320e2
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2013-04-10 15:28:18 -0400

    adding the start of the testing suite for numerics properties

---
+++ tests/properties.hs
@@ -0,0 +1,12 @@
+
+
+module Main where
+
+
+import Control.Applicative
+import Control.Lens
+import Test.QuickCheck
+import Test.QuickCheck.Function
+import Test.Framework.TH
+import Test.Framework.Providers.QuickCheck2
+
