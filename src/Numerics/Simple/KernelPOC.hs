db73edf5220cdeda8cbbb06a3e2efa5f44d06b15
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2013-04-29 01:18:28 -0400

    still something wrong with this algorithm..

--- src/Numerics/Simple/KernelPOC.hs
+++ src/Numerics/Simple/KernelPOC.hs
@@ -105,10 +105,10 @@ simpleLooper !rMat !aMat !bMat !n = go 0 0 0  0  --- we're about to run step 0!!
     where 
         !blockedN = n `div` 4 --- 4x4
         !blockCubed = blockedN * blockedN * blockedN
-        go !x !y !z   !count |   x < (blockedN-1) || y < (blockedN-1) || z < (blockedN-1) 
+        go !x !y !z   !count |  ( x < (blockedN-1) || y < (blockedN-1) || z < (blockedN-1) ) || (count < blockCubed)
                 =  --- if we hit the bounds all at once, we win!
                          do   
-                            appKernel64 idKernel x y z  rMat aMat bMat
+                            appKernel64 idKernel  x y z  rMat aMat bMat
                             next x y z (count + 1)
                     | otherwise =  
                             do 
